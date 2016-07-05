14dd52e3c4c7b14bcfad94eaad4eebab6a805e5a
Brad <brad@bradbrothers.ca>
2016-07-04 22:59:27 -0400

    moved assets to resources directory and added gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,19 @@
+require('laravel-elixir-imagemin');
 var elixir = require('laravel-elixir');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
-
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.styles([
+        'bootstrap.css',
+        'landing-page.css',
+        'font-awesome.css',
+        'lato.css'
+    ]);
+    mix.scripts([
+        'jquery.js',
+        'bootstrap.js',
+        'app.js'
+    ]);
+    mix.imagemin();
+    mix.version(['css/all.css', 'js/all.js', 'public/images']);
 });
+
