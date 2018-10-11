46962dda0fb399209cef7691ba3f6221825a87f6
Caryn <c.humphreys@vehikl.com>
2018-10-11 12:55:05 -0400

    a wee bit more prep

--- dev-ldn/src/App.js
+++ dev-ldn/src/App.js
@@ -7,7 +7,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <button>join slack</button>
+          <button>join meetup</button>
         </header>
+        <body>div.</body>
       </div>
     );
   }
