b097720e8e66a860f143984168b161abcb607afb
Caryn <c.humphreys@vehikl.com>
2018-11-12 14:42:53 -0500

    adds november sponsors

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -1,21 +1,53 @@
 import React, { Component } from 'react';
 import vehikl from '../assets/vehikl-square.jpg';
+import wes from '../assets/wes-bos.jpg';
+import ztr from '../assets/ztr.png';
 
 class Sponsors extends Component {
   render() {
     return (
       <div className="sponsor">
-        <p>Thank you to the generosity of our sponsors</p>
-        <div className="sponsor-container">
-          <a
-            href="https://vehikl.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className="logo grow shadow">
-              <img src={vehikl} alt="Vehikl logo" />
+        <div className="sponsors-container">
+          <div className="current">
+            <h1>Current Sponsors</h1>
+            <p>
+              This group is made possible by the generosity of our monthly
+              sponsors.
+            </p>
+            <div className="sponsors">
+              <a
+                href="https://wesbos.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="logo grow shadow wes">
+                  <img src={wes} alt="Wes Bos logo" />
+                </div>
+              </a>
+              <a
+                href="http://ztr.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="logo grow shadow">
+                  <img src={ztr} alt="ZTR Control Systems logo" />
+                </div>
+              </a>
             </div>
-          </a>
+          </div>
+          <div className="past">
+            <h2>Past Sponsors</h2>
+            <div className="sponsors-list">
+              <a
+                href="https://vehikl.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Vehikl
+              </a>{' '}
+              (venue sponsor)
+            </div>
+          </div>
         </div>
       </div>
     );
